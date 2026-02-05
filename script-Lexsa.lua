de7670a728b0ad129ba8109bfb5c0fee62d32a27
ersyabengkulu-oss <ersyabengkulu@gmail.com>
2026-02-05 22:17:10 +0700

    Update script-Lexsa.lua

--- script-Lexsa.lua
+++ script-Lexsa.lua
@@ -1,12 +1,19 @@
 -- Notifikasi Awal
 game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Lexsa Script";
-    Text = "Fitur Speed & Jump Aktif!";
+    Title = "Lexsa Script V2";
+    Text = "Speed, Jump, & Fly Aktif!";
     Duration = 5;
 })
 
--- Fitur Speed (Kecepatan Lari)
+-- Fitur Speed & Jump
 game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
-
--- Fitur Jump (Kekuatan Lompat)
 game.Players.LocalPlayer.Character.Humanoid.JumpPower = 100
+
+-- Fitur Infinite Jump (Lompat di Udara)
+game:GetService("UserInputService").JumpRequest:Connect(function()
+    game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):ChangeState("Jumping")
+end)
+
+-- Fitur Fly (Tekan E untuk Terbang/Berhenti)
+-- Catatan: Fitur ini biasanya butuh script tambahan yang panjang, 
+-- tapi untuk sekarang, Infinite Jump sudah membuatmu bisa "terbang" dengan menekan spasi terus-menerus!
