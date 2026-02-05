727499071788722b996b090062b286a316d1e23d
ersyabengkulu-oss <ersyabengkulu@gmail.com>
2026-02-06 06:16:44 +0700

    Update script-Lexsa.lua

--- script-Lexsa.lua
+++ script-Lexsa.lua
@@ -1,30 +1,13 @@
--- LEXSA V7.5: FIX REMOTE SEQUENCE
-local catching = false
-CatchBtn.MouseButton1Click:Connect(function()
-    catching = not catching
-    CatchBtn.Text = catching and "FIXED: ON" or "FIXED: OFF"
+-- LEXSA V7.6: THE MIRROR MODE
+pcall(function()
+    local RS = game:GetService("ReplicatedStorage")
+    -- Kita ambil remotenya langsung dari memory
+    local catch = RS:FindFirstChild("CatchFishComplete", true)
     
-    task.spawn(function()
-        local RS = game:GetService("ReplicatedStorage")
-        -- Cari folder remote sesuai game pancingmu
-        local cast = RS:FindFirstChild("ChargeFishingRod", true)
-        local req = RS:FindFirstChild("RequestFishingMinigame", true)
-        local catch = RS:FindFirstChild("CatchFishComplete", true)
-        
-        while catching do
-            pcall(function()
-                -- 1. Lempar (Sesuai urutan log 1)
-                cast:InvokeServer(100)
-                task.wait(0.2) 
-                
-                -- 2. Request Minigame (Sesuai urutan log 2)
-                req:InvokeServer()
-                task.wait(0.2)
-                
-                -- 3. Klaim Ikan (Sesuai urutan log 3)
-                catch:InvokeServer(true)
-                task.wait(0.5) -- Beri waktu server bernapas
-            end)
-        end
-    end)
+    -- Kita paksa kirim sinyal 'Selesai' berkali-kali
+    for i = 1, 5 do
+        task.spawn(function()
+            catch:InvokeServer(true)
+        end)
+    end
 end)
