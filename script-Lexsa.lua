4a4a3981ac45cafb8acc42be7f3807cc25cf6977
ersyabengkulu-oss <ersyabengkulu@gmail.com>
2026-02-05 22:15:09 +0700

    Update notification text and add speed and jump features

--- script-Lexsa.lua
+++ script-Lexsa.lua
@@ -1,6 +1,12 @@
+-- Notifikasi Awal
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "Lexsa Script";
-    Text = "Hore! Script sudah jalan!";
+    Text = "Fitur Speed & Jump Aktif!";
     Duration = 5;
 })
 
+-- Fitur Speed (Kecepatan Lari)
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
+
+-- Fitur Jump (Kekuatan Lompat)
+game.Players.LocalPlayer.Character.Humanoid.JumpPower = 100
