dee2396fc98be431f2a132d421ca59292501de22
ersyabengkulu-oss <ersyabengkulu@gmail.com>
2026-03-08 18:55:51 +0700

    Update script-Lexsa.lua

--- script-Lexsa.lua
+++ script-Lexsa.lua
@@ -1,17 +1,17 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Lexsa Multi-Waypoints", "DarkTheme")
 
--- Tab Khusus Simpan
+-- Tab Simpan
 local TabSet = Window:NewTab("SET (Simpan)")
-local SetSection = TabSet:NewSection("Klik untuk Simpan Titik Aman")
+local SetSection = TabSet:NewSection("Klik untuk Simpan Posisi")
 
--- Tab Khusus Teleport
+-- Tab Pindah
 local TabGo = Window:NewTab("TELE (Pindah)")
-local GoSection = TabGo:NewSection("Klik untuk Teleport Balik")
+local GoSection = TabGo:NewSection("Klik untuk Teleport")
 
 local Positions = {}
 
--- Fungsi Bikin 5 Slot
+-- Bikin 5 Slot
 for i = 1, 5 do
     SetSection:NewButton("Amankan Slot "..i, "Simpan koordinat sekarang", function()
         Positions[i] = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
@@ -27,12 +27,13 @@ for i = 1, 5 do
     end)
 end
 
--- FITUR BUKA TUTUP (Toggle)
-local TabSettings = Window:NewTab("TUTUP")
-local SettSection = TabSettings:NewSection("Buka/Tutup Menu")
-SettSection:NewKeybind("Sembunyikan Menu", "Tekan P atau klik ini", Enum.KeyCode.P, function()
+-- FITUR TUTUP/BUKA (Toggle)
+local TabTutup = Window:NewTab("TUTUP")
+local TutupSection = TabTutup:NewSection("Pengaturan Menu")
+
+TutupSection:NewKeybind("Buka/Tutup Menu", "Pencet P untuk sembunyiin", Enum.KeyCode.P, function()
 	Library:ToggleLib()
 end)
 
--- Tombol Melayang Otomatis (Buat Mobile)
--- Jika GUI ngalangin, pencet tombol Settings tadi atau tekan P
+-- Biar gak cape nulis, ini otomatis bikin tombol melayang di HP
+-- Jadi kalau menu ditutup, ada tombol kecil buat buka lagi
