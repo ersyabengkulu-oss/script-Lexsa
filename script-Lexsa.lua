8f1d39bdcba24216a63b39eb11e21ca6cb8f1a20
ersyabengkulu-oss <ersyabengkulu@gmail.com>
2026-03-08 18:58:51 +0700

    Update script-Lexsa.lua

--- script-Lexsa.lua
+++ script-Lexsa.lua
@@ -1,39 +1,36 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+-- Bikin Window dengan posisi di tengah layar
 local Window = Library.CreateLib("Lexsa Multi-Waypoints", "DarkTheme")
 
--- Tab Simpan
-local TabSet = Window:NewTab("SET (Simpan)")
-local SetSection = TabSet:NewSection("Klik untuk Simpan Posisi")
+-- Tab khusus buat simpan koordinat
+local TabSet = Window:NewTab("SET POSISI")
+local SetSection = TabSet:NewSection("Pencet buat simpan titik aman")
 
--- Tab Pindah
-local TabGo = Window:NewTab("TELE (Pindah)")
-local GoSection = TabGo:NewSection("Klik untuk Teleport")
+-- Tab khusus buat pindah tempat
+local TabGo = Window:NewTab("TELEPORT")
+local GoSection = TabGo:NewSection("Pencet buat balik ke titik tadi")
 
 local Positions = {}
 
--- Bikin 5 Slot
+-- Bikin 5 Slot otomatis biar rapi
 for i = 1, 5 do
-    SetSection:NewButton("Amankan Slot "..i, "Simpan koordinat sekarang", function()
+    SetSection:NewButton("Simpan Titik Slot "..i, "Tandai tempat ini", function()
         Positions[i] = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-        Library:Notify("Lexsa", "Slot "..i.." Berhasil Disimpan!", 2)
+        Library:Notify("Lexsa Info", "Slot "..i.." Berhasil Disimpan!", 2)
     end)
 
-    GoSection:NewButton("Balik ke Slot "..i, "Pindah ke titik "..i, function()
+    GoSection:NewButton("Balik ke Slot "..i, "Teleport sekarang", function()
         if Positions[i] then
             game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Positions[i]
         else
-            Library:Notify("Gagal", "Isi dulu slot "..i.." di Tab SET!", 2)
+            Library:Notify("Gagal", "Simpan posisi dulu di tab SET!", 2)
         end
     end)
 end
 
--- FITUR TUTUP/BUKA (Toggle)
+-- Tab buat buka tutup GUI
 local TabTutup = Window:NewTab("TUTUP")
-local TutupSection = TabTutup:NewSection("Pengaturan Menu")
-
-TutupSection:NewKeybind("Buka/Tutup Menu", "Pencet P untuk sembunyiin", Enum.KeyCode.P, function()
-	Library:ToggleLib()
+local TutupSection = TabTutup:NewSection("Klik 'Sembunyikan' buat hide menu")
+TutupSection:NewKeybind("Sembunyikan Menu", "Pencet P atau klik toggle", Enum.KeyCode.P, function()
+    Library:ToggleLib()
 end)
-
--- Biar gak cape nulis, ini otomatis bikin tombol melayang di HP
--- Jadi kalau menu ditutup, ada tombol kecil buat buka lagi
